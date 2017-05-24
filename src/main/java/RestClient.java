164c0db6b06f56a3974736731abedb6b95d229d4
Alvaro Molina <alvaromolinac@gmail.com>
2017-05-24 15:29:04 -0400

    prueba thread

--- src/main/java/RestClient.java
+++ src/main/java/RestClient.java
@@ -1,40 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 public class RestClient {
 
     public static void main(String[] args) {
         try {
 
-            DefaultHttpClient httpClient = new DefaultHttpClient();
+            while(true){
 
-            HttpGet getRequest = new HttpGet(
-                    "http://localhost:8080/stepLogs");
-            getRequest.addHeader("accept", "application/json");
+                DefaultHttpClient httpClient = new DefaultHttpClient();
 
-            HttpResponse response = httpClient.execute(getRequest);
+                //HttpGet getRequest = new HttpGet(
+                //        "http://localhost:8080/stepLogs");
 
-            if (response.getStatusLine().getStatusCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatusLine().getStatusCode());
+                HttpPost postRequest = new HttpPost(
+                        "http://localhost:8080/stepLog");
+
+                ArrayList<NameValuePair> postParameters;
+
+
+                postParameters = new ArrayList<NameValuePair>();
+                postParameters.add(new BasicNameValuePair("steps", "20"));
+                postRequest.setEntity(new UrlEncodedFormEntity(postParameters));
+
+
+
+
+                postRequest.addHeader("accept", "application/json");
+
+                HttpResponse response = httpClient.execute(postRequest);
+
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + response.getStatusLine().getStatusCode());
+                }
+
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader((response.getEntity().getContent())));
+
+                String output;
+                System.out.println("Output from Server .... \n");
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                httpClient.getConnectionManager().shutdown();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader((response.getEntity().getContent())));
 
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
 
-            httpClient.getConnectionManager().shutdown();
 
         } catch (ClientProtocolException e) {
 
